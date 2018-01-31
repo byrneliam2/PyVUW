e363998599c0760def4bd4aaf5ab027f31481fd8
Liam <liam.jb1@gmail.com>
2018-01-31 21:33:33 +1300

    Additional delete syntax and print refinement

--- pyvuw/user/ui.py
+++ pyvuw/user/ui.py
@@ -23,13 +23,22 @@ class UI:
     def do_next(self):
         """
         Take a user input and perform the operation it translates to, if it is correct.
-        TODO split this out to a controller?
         """
+
+        # get the input and split into a list of arguments
         args = input("> ").split()
+
+        # determine action from input
+        # commands with arguments
         if args[0] == "a":
             self._org.add_data(args[1], args[2:])
         elif args[0] == "del":
+            # special case: either we can delete by specifying the exact string to delete
+            # or the index of the item to delete
+            if args[1].startswith("["):
+                pass
             self._org.del_data(args[1], args[2:])
+        # no argument commands
         elif args[0] == "man":
             self.print_man()
         elif args[0] == "s":
@@ -53,15 +62,18 @@ class UI:
         """
         Reprint the entire organiser.
         """
+
+        def print_lines():
+            print("-" * self.NUM_CNAME, end='')
+            print("-" * self.NUM_CWORK * len(work))
+
         print("=" * self.NUM_TNAME + " PyVUW Organiser " + "=" * self.NUM_TNAME)
         print()
         print("(" + str(len(self._org)) + " courses, " + str(self._org.total_work()) + " tasks)")
         for name, work in self._org:
-            print("-" * self.NUM_CNAME, end='')
-            print("-" * self.NUM_CWORK * len(work))
+            print_lines()
             print(name + ": ", end='')
             for w in work:
                 print(w + " ", end='')
             print()
-            print("-" * self.NUM_CNAME, end='')
-            print("-" * self.NUM_CWORK * len(work))
+            print_lines()
